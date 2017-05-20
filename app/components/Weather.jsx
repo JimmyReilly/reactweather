5c3a1b8a60890e47bc84e16ac362b59806ada055
reilly472 <reilly@whitewavemedia.com>
2017-05-20 10:45:25 -0400

    wiring up other search bar

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -19,7 +19,9 @@ var Weather = React.createClass({
         // debugger; This sets a break point
         this.setState({
             isLoading: true,
-            errorMessage: undefined
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
 
         openWeatherMap.getTemp(location).then(function(temp) {
@@ -35,6 +37,22 @@ var Weather = React.createClass({
             });
             //alert(errorMessage);
         });
+    },
+    componentWillReceiveProps: function(newProps){
+        var location = newProps.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
+    },
+    componentDidMount: function () {
+        var location = this.props.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
     },
     render: function(){
         var {isLoading, temp, location, errorMessage} = this.state;
